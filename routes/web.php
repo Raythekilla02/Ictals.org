157cf33c4c2b3a9125a446ae034b79ea10b8534d
Raja Quraish <rajaq1441@gmail.com>
2026-04-03 13:03:41 -0400

    Made changes to homepage layout and added few more features

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,34 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+Route::view('/archive', 'archive');
 
 // Route::statamic('example', 'example-view', [
 //    'title' => 'Example'
 // ]);
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Http\Request;
+
+Route::post('/send-message', function (Request $request) {
+    $data = $request->validate([
+        'name' => 'required',
+        'phone' => 'required',
+        'email' => 'required|email',
+        'message' => 'required',
+    ]);
+
+    // Send email to YOU
+    Mail::raw(
+        "New message from ICTALS website:\n\n" .
+        "Name: {$data['name']}\n" .
+        "Phone: {$data['phone']}\n" .
+        "Email: {$data['email']}\n\n" .
+        "Message:\n{$data['message']}",
+        function ($msg) {
+            $msg->to('YOUR_EMAIL_HERE')
+                ->subject('New Contact Form Message');
+        }
+    );
+
+    return back()->with('success', 'Your message has been sent!');
+});
