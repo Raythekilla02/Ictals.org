e167a3dc5ae0ca16c8bbf127f08891747d52154b
Raja Quraish <rajaq1441@gmail.com>
2026-05-20 20:49:06 -0400

    Modified the provider section

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,6 +19,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
-    }
+public function boot(): void
+{
+    \Statamic\CP\Utilities\Utility::register(\App\Utilities\LaunchEmailUtility::class);
+}    
 }
