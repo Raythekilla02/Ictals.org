db51368299b129bef5acb55c66add7d664b159e3
Raja Quraish <rajaq1441@gmail.com>
2026-05-24 20:49:42 -0400

    Redid the whole serviceprovide file

--- addons/LaunchEmail/src/ServiceProvider.php
+++ addons/LaunchEmail/src/ServiceProvider.php
@@ -3,18 +3,10 @@
 namespace LaunchEmail;
 
 use Statamic\Providers\AddonServiceProvider;
-use Statamic\Facades\Utility;
 
 class ServiceProvider extends AddonServiceProvider
 {
     protected $nav = [
         \LaunchEmail\CpNav::class,
     ];
-
-    public function bootAddon()
-    {
-        Utility::register(LaunchEmailUtility::class)
-            ->title('Launch Email')
-            ->icon('email');
-    }
 }
