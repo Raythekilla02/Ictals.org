c92e07fdcaf006df5086545c03b2fe3236afd447
Raja Quraish <rajaq1441@gmail.com>
2026-05-25 15:45:22 -0400

    Added few things to appserviceprovider file

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Statamic\Facades\CP\Nav;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,6 +14,10 @@ class AppServiceProvider extends ServiceProvider
 
     public function boot(): void
     {
-        //
+        Nav::extend(function ($nav) {
+            $nav->content('Zoho Mail Login')
+                ->url('https://www.zoho.com/mail/login.html')
+                ->icon('mail');
+        });
     }
 }
